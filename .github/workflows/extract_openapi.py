487e7b317203bab98c2049f601fa5e6f2168c497
Ubuntu <ubuntu@ip-172-31-4-151.ec2.internal>
2022-07-14 13:59:55 +0000

    check secretts before docker build

--- .github/workflows/extract_openapi.py
+++ .github/workflows/extract_openapi.py
@@ -5,4 +5,4 @@ with open("inspect.json", "r") as f:
     data = json.load(f)[0]
 
 with open("openapi.json", "w") as f:
-   f.write(data["ContainerConfig"]["Labels"]["org.cogmodel.openapi_schema"])
+    f.write(data["ContainerConfig"]["Labels"]["org.cogmodel.openapi_schema"])
